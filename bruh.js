4b8b8c85eff148c3cd744754057cca260155475d
Zaphod <sfolk55@gmail.com>
2022-12-12 21:34:44 +0100

    Removed console log

--- bruh.js
+++ bruh.js
@@ -16,7 +16,6 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   	if(channel && channel.name.toLowerCase().includes('bruh')) {
 		channel.join().then(connection =>{
 			const dispatcher = connection.play(require("path").join(__dirname,'./bruh.mp3'));
-			console.log(dispatcher);
 
 			dispatcher.on('finish', () => {
 				console.log('end');
