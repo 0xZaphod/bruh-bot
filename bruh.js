aafbdc5f2efedaea4a2ffddf387af0c8a9eab304
Zaphod <sfolk55@gmail.com>
2023-03-20 22:22:26 +0100

    Update node, discordjs... (pain)

--- bruh.js
+++ bruh.js
@@ -1,30 +1,47 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
+const { GatewayIntentBits } = require('discord.js');
 
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+const { joinVoiceChannel, VoiceConnectionStatus, createAudioPlayer, AudioPlayerStatus, createAudioResource, entersState } = require('@discordjs/voice');
 
 client.on('ready', () => {
-	console.log('ready');
+    console.log('ready');
 });
 
 client.login(process.env.token);
 
-client.on('voiceStateUpdate', (oldState, newState) => {
-	let oldChannel = oldState.channel;
-	let newChannel = newState.channel;
+const resource = createAudioResource('bruh.mp3');
+const player = createAudioPlayer();
 
-  	if(newChannel 
-	   && oldChannel != newChannel 
-	   && newChannel.name.toLowerCase().includes('bruh')) {
-		newChannel.join().then(connection =>{
-			const dispatcher = connection.play(require("path").join(__dirname,'./bruh.mp3'));
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    let oldChannel = oldState.channel;
+    let newChannel = newState.channel;
 
-			dispatcher.on('finish', () => {
-				console.log('end');
-				dispatcher.destroy();
-				newChannel.leave();
-			});
-		});
-  	} 
+    if (newChannel
+        && oldChannel?.id != newChannel?.id
+        && newChannel?.name.toLowerCase().includes('bruh')) {
+            const connection = joinVoiceChannel({
+                channelId: newChannel.id,
+                guildId: newChannel.guild.id,
+                adapterCreator: newChannel.guild.voiceAdapterCreator,
+            });
+
+
+            connection.on(VoiceConnectionStatus.Ready, () => {
+                connection.subscribe(player);
+                player.play(resource);
+            });
+
+            player.on('error', error => {
+                console.error(`Error: ${error.message}`);
+            })
+
+            player.on(AudioPlayerStatus.Idle, ()=> {
+                player.stop();
+                connection.destroy();
+            })
+    }
 });
