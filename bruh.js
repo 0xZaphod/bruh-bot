cbb47f22022d8149286ac26bdfe7dd4f6dcd526a
Zaphod <sfolk55@gmail.com>
2024-08-04 11:06:26 +0200

    Check state before destroying connection

--- bruh.js
+++ bruh.js
@@ -23,25 +23,26 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if (newChannel
         && oldChannel?.id != newChannel?.id
         && newChannel?.name.toLowerCase().includes('bruh')) {
-            const connection = joinVoiceChannel({
-                channelId: newChannel.id,
-                guildId: newChannel.guild.id,
-                adapterCreator: newChannel.guild.voiceAdapterCreator,
-            });
+        const connection = joinVoiceChannel({
+            channelId: newChannel.id,
+            guildId: newChannel.guild.id,
+            adapterCreator: newChannel.guild.voiceAdapterCreator,
+        });
 
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            connection.subscribe(player);
+            player.play(resource);
+        });
 
-            connection.on(VoiceConnectionStatus.Ready, () => {
-                connection.subscribe(player);
-                player.play(resource);
-            });
+        player.on('error', error => {
+            console.error(`Error: ${error.message}`);
+        })
 
-            player.on('error', error => {
-                console.error(`Error: ${error.message}`);
-            })
-
-            player.on(AudioPlayerStatus.Idle, ()=> {
-                player.stop();
+        player.on(AudioPlayerStatus.Idle, () => {
+            player.stop();
+            if (connection.state.status !== VoiceConnectionStatus.Destroyed) {
                 connection.destroy();
-            })
+            }
+        })
     }
 });
