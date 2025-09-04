9924ef1cbdaf04bb6f4b37a700f38c1c97b2002b
Zaphod <sfolk55@gmail.com>
2025-09-04 20:38:49 +0200

    Create new resource and player every connection

--- bruh.js
+++ bruh.js
@@ -13,8 +13,6 @@ client.on('ready', () => {
 
 client.login(process.env.token);
 
-const resource = createAudioResource('bruh.mp3');
-const player = createAudioPlayer();
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
     let oldChannel = oldState.channel;
@@ -23,11 +21,15 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if (newChannel
         && oldChannel?.id != newChannel?.id
         && newChannel?.name.toLowerCase().includes('bruh')) {
+
         const connection = joinVoiceChannel({
             channelId: newChannel.id,
             guildId: newChannel.guild.id,
             adapterCreator: newChannel.guild.voiceAdapterCreator,
         });
+        
+        const resource = createAudioResource('bruh.mp3');
+        const player = createAudioPlayer();
 
         connection.on(VoiceConnectionStatus.Ready, () => {
             connection.subscribe(player);
@@ -36,13 +38,14 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 
         player.on('error', error => {
             console.error(`Error: ${error.message}`);
-        })
+            connection.destroy();
+        });
 
         player.on(AudioPlayerStatus.Idle, () => {
             player.stop();
             if (connection.state.status !== VoiceConnectionStatus.Destroyed) {
                 connection.destroy();
             }
-        })
+        });
     }
 });
